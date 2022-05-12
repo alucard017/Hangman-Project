a57d6d5f0465d2ddb39ead3fb069d17fa7d73994
Apurba Sundar Nayak <76940364+momentsofjoy@users.noreply.github.com>
2022-05-12 15:25:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+from hangman_art import stages, logo
+from hangman_words import word_list
+from replit import clear
+
+print(logo)
+
+    #Use the clear() function imported from replit to clear the output between guesses.
